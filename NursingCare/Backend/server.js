fb62730d1f39cc3c73a6587977b28ef225d8d482
Joel Mbithi <joellembithi@gmail.com>
2025-02-21 15:04:13 +0300

    testing the api using thunder cline

--- NursingCare/Backend/server.js
+++ NursingCare/Backend/server.js
@@ -1,18 +1,22 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
 //API config
 const app = express()
 const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
 
 // middlewares
 app.use(express.json())
 app.use(cors())
 
 //End Point
-
+app.use('/api/admin',adminRouter)
 app.get('/',(req,res) => {
      res.send('API WORKING WELL')
 })
