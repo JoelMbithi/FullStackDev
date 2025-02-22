38cba01415d860ec646e63059b9e1443dd9f126f
Joel Mbithi <joellembithi@gmail.com>
2025-02-22 09:58:30 +0300

    start of project

---
+++ learningapp/src/app/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <div>
+        <div>
+          <p className="text-xl size-max text-blue-600">jpe</p>
+        </div>
+        <Button className="bg-red-600"> click me</Button>
+     
+      <p className="text-xl size-max text-blue-600">jpe</p>
+    
+    <Button className="bg-red-600"> click me</Button>
+  </div>
+  );
+}
+
