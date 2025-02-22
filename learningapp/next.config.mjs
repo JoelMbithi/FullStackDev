71e3bea7408efb54b40ee86c05a0af1eb913aa03
Joel Mbithi <joellembithi@gmail.com>
2025-02-22 19:50:18 +0300

    Clerk Configuration for Auth

---
+++ learningapp/next.config.mjs
@@ -0,0 +1,9 @@
+import pkg from "next";
+const { NextConfig } = pkg;
+
+/** @type {NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default nextConfig;
--- learningapp/src/app/(Dashboard)/(routes)/page.tsx
+++ learningapp/src/app/(Dashboard)/(routes)/page.tsx
@@ -5,13 +5,11 @@ export default function Home() {
   return (
       <div>
         <div>
-          <p className="text-xl size-max text-blue-600">jpe</p>
+          <p className="text-xl size-max text-blue-600">joe</p>
         </div>
         <Button className="bg-red-600"> click me</Button>
      
-      <p className="text-xl size-max text-blue-600">jpe</p>
-    
-    <Button className="bg-red-600"> click me</Button>
+     
   </div>
   );
 }
