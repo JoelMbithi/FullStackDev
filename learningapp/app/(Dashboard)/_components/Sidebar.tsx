354886a00eb70a6aa76388799b9acbae5af6f032
Joel Mbithi <joellembithi@gmail.com>
2025-02-23 12:36:32 +0300

    doing Api for the sidebare

--- learningapp/app/(Dashboard)/_components/Sidebar.tsx
+++ learningapp/app/(Dashboard)/_components/Sidebar.tsx
@@ -1,9 +1,14 @@
 import { Logo } from "./Logo";
+import SidebarRoutes from "./SidebarRoutes";
 const Sidebar = () => {
     return ( 
-        <div className="h-full border-r flex flex-col overflow-y-auto bg-teal-50 shadow-sm">
-          <div className="p-6">
-            <Logo />
+        <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+          <div className="p-6 flex ">
+            <Logo /> 
+            <h1 className="p-2 mt-2 text-bold text-">Learning App</h1>
+          </div>
+          <div className=" flex flex-col w-full">
+             <SidebarRoutes />
           </div>
         </div>
      );
---
+++ learningapp/app/(Dashboard)/_components/SidebarRoutes.tsx
@@ -0,0 +1,37 @@
+"use client"
+import { Compass, Layout } from "lucide-react";
+import { SidebarItem } from "./SidebarItem";
+
+
+
+
+const guestRoutes = [
+{
+    icon: Layout,
+    label: "Dashboard",
+    href:"/"
+},
+{
+    icon: Compass,
+    label: "Browser",
+    href:"/search"
+}
+]
+
+const SidebarRoutes = () => {
+    const routes = guestRoutes;
+    return ( 
+        <div  className="flex flex-col w-full">
+            {routes.map((route) => (
+                <SidebarItem
+                 key={route.href}
+                label={route.label}
+                icon={route.icon}
+                 href={route.href}
+                 />
+            ))}
+        </div>
+     );
+}
+ 
+export default SidebarRoutes;
