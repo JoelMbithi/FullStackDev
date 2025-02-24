6454fc7718c009e444a4b7f1a9eb97b2bea64502
Joel Mbithi <joellembithi@gmail.com>
2025-02-24 13:07:33 +0300

    doing Navbar Routes for dinamic components

--- learningapp/app/page.tsx
+++ learningapp/app/page.tsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
+"use client";  
+
+import { UserButton, useUser } from "@clerk/nextjs";
 import DashboardLayout from "./(Dashboard)/layout";
 
 export default function Home() {
+  const { user } = useUser(); 
+
   return (
     <DashboardLayout>
-    <div>Content Here</div>
-  </DashboardLayout>
+      <div className="flex items-center gap-4 p-6">
+        {/* UserButton will handle profile image & logout */}
+        {user && <UserButton afterSignOutUrl="/" />}
+        
+        <h1 className="text-xl font-bold">Welcome, {user?.firstName}!</h1>
+      </div>
+    </DashboardLayout>
   );
 }
