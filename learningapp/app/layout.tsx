069a214c398383bf422e7242228a34a0f86a12a3
Joel Mbithi <joellembithi@gmail.com>
2025-02-26 18:02:12 +0300

    done the form to create new course now i need to create the api to handle that with database

--- learningapp/app/layout.tsx
+++ learningapp/app/layout.tsx
@@ -6,6 +6,7 @@ import {
   UserButton
 } from '@clerk/nextjs'
 import './globals.css'
+import { ToasterProvider } from '@/components/Providers/toaster-provider'
 export default function RootLayout({
   children,
 }: {
@@ -21,6 +22,7 @@ export default function RootLayout({
           <SignedIn>
             <UserButton />
           </SignedIn>
+          <ToasterProvider />
           {children}
         </body>
       </html>
