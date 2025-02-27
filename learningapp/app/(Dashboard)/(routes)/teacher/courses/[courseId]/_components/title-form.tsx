2ad6a891bde060d12ffbbb03d3c9402431b7019c
Joel Mbithi <joellembithi@gmail.com>
2025-02-27 20:57:40 +0300

    Modifield the my Form

--- learningapp/app/(Dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ learningapp/app/(Dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { FormControl, FormField, FormItem } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Pencil } from "lucide-react";
 import { useState } from "react";
-import { Form, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Form, FormField, FormItem, FormControl } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface TitleFormProps {
   initialData: {
@@ -43,49 +43,46 @@ export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
   const toggleEdit = () => setIsEditing((current) => !current);
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
+     
       <div className="flex items-center justify-between">
-        <h2 className="text-lg font-semibold flex items-center gap-2">
-          Course Title
-          <Button onClick={toggleEdit} variant="ghost">
-            {isEditing ? (
-              <>Cancel</>
-            ) : (
-              <>
-                <Pencil className="h-4 w-4 mt-2" />
-                Edit title
-              </>
-            )}
-          </Button>
-        </h2>
-      </div>
-      {!isEditing && <p className="text-sm mt-2">{initialData.title}</p>}
-      {isEditing && (
+      <h2 className="text-lg font-semibold flex items-center gap-2">
+      Course Title
+      <Button onClick={toggleEdit} variant="ghost">
+        {isEditing ? (
+          <>Cancel</>
+        ) : (
+          <>
+            <Pencil className="h-4 w-4 mt-2" />
+            Edit title
+          </>
+        )}
+      </Button>
+      </h2>
+    </div>
+    { !isEditing && (
+        <p className="text-sm mt-2">
+        {initialData.title}
+        </p>
+    )}
+    {isEditing && (
         <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-4 mt-4"
-          >
-            <FormField
-              control={form.control}
-              name="title"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      disabled={isSubmitting}
-                      {...field}
-                      placeholder="Course Title"
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <Button type="submit" disabled={isSubmitting}>
-              Save
-            </Button>
-          </form>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mt-4">
+              
+                   <FormField
+                   control={form.control} 
+                   name= 'title'
+                   render={({field}) => (
+                      <FormItem>
+                          <FormControl>
+                              <Input disabled={isSubmitting} {...field} placeholder="Course Title" />
+                          </FormControl>
+                      </FormItem>
+                   )}
+                   />
+                
+            </form>
         </Form>
-      )}
+    )}
     </div>
   );
-};
+}; 
