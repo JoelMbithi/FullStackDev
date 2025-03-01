38e5809462164809a505af9821b5533f00e17fa9
Joel Mbithi <joellembithi@gmail.com>
2025-03-02 01:28:52 +0300

    i will continue

--- learningapp/app/(Dashboard)/(routes)/teacher/courses/page.tsx
+++ learningapp/app/(Dashboard)/(routes)/teacher/courses/page.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 const CoursePage = () => {
     return ( 
         <div className="p-2">
-              <Link href="/teacher/create">        
-             <Button>
-                New Course
-             </Button>
-             </Link>
+            <Link href="/teacher/create" passHref>
+                <Button asChild>
+                    <span>New Course</span>
+                </Button>
+            </Link>
         </div>
-     );
+    );
 }
- 
+
 export default CoursePage;
--- learningapp/app/api/uploadthing/core.ts
+++ learningapp/app/api/uploadthing/core.ts
@@ -1,12 +1,10 @@
 import { auth } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
 const handleAuth = async () => {
     const { userId } = await auth();
-
     if (!userId) throw new Error("Unauthorized");
     return { userId };
 };
@@ -14,18 +12,24 @@ const handleAuth = async () => {
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
     courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1}})
-    .middleware(() => handleAuth())
-    .onUploadComplete(() => {}),
+      .middleware(async () => await handleAuth()) 
+      
+      .onUploadComplete(({ file }) => { 
+        
+        console.log("✅ Course Image Uploaded:", file);
+      }),
+
     courseAttachment: f(["text", "image", "video", "audio", "pdf"])
-    .middleware(() => handleAuth())
-    .onUploadComplete(() => {}),
-    chapterVideo:f({video: {maxFileCount: 1, maxFileSize: "512GB"}})
-    .middleware(()=> handleAuth())
-    .onUploadComplete(() => {}),
-    
+      .middleware(async () => await handleAuth()) 
+      .onUploadComplete(({ file }) => { 
+        console.log("✅ Course Attachment Uploaded:", file);
+      }),
 
-
+    chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: "512GB" }})
+      .middleware(async () => await handleAuth()) 
+      .onUploadComplete(({ file }) => { 
+        console.log("✅ Chapter Video Uploaded:", file);
+      }),
 } satisfies FileRouter;
-
 
 export type OurFileRouter = typeof ourFileRouter;
