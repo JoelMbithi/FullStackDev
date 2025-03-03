6c65dbb2706c480d64ec3eb0e9030851337b4680
Joel Mbithi <joellembithi@gmail.com>
2025-03-03 12:02:06 +0300

    Done the user hotwl models and verification

--- API_learn/api/routes/hotelRoute.js
+++ API_learn/api/routes/hotelRoute.js
@@ -1,17 +1,19 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
 import { createHotel, deleteHotel, getHotels, updatedHotel } from "../controller/hotelController.js";
+import { verifyAdmin } from "../utils/verifyToken.js"
+
 
 const router = express.Router();
 
 //API TO CREATE hotel
-router.post("/", createHotel)
+router.post("/",verifyAdmin, createHotel)
 
 /// API to Update Hotel
-router.put("/:id",updatedHotel) 
+router.put("/:id",verifyAdmin,updatedHotel) 
 //API to DELETE
 
-router.delete("/:id",deleteHotel)
+router.delete("/",verifyAdmin,deleteHotel)
 
 //API TO GET ALL
 router.get("/:id", getHotels)
--- API_learn/api/utils/verifyToken.js
+++ API_learn/api/utils/verifyToken.js
@@ -2,24 +2,50 @@ import jwt from "jsonwebtoken";
 import { createError } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
-    let token = req.cookies?.access_token;
+    const token = req.cookies?.access_token || req.headers.authorization?.split(" ")[1];
 
-    // If token is not in cookies, check Authorization header
-    if (!token && req.headers.authorization) {
-        const authHeader = req.headers.authorization;
-        if (authHeader.startsWith("Bearer ")) {
-            token = authHeader.split(" ")[1];
-        }
-    }
+    console.log("Received Token:", token); // Debugging
 
     if (!token) {
         return next(createError(401, "You are not authenticated!"));
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
-        if (error) return next(createError(403, "Token is not valid!"));
-        
+        if (error) {
+            console.log("JWT Error:", error.message); // Debugging
+            return next(createError(403, "Token is not valid!"));
+        }
+
         req.user = user;  // Attach user info to request object
+        console.log("Decoded User:", req.user); // Debugging
         next(); // Proceed to next middleware
+    });
+};
+
+// Middleware to verify user permissions (for deleting/updating user)
+export const verifyUser = (req, res, next) => {
+    verifyToken(req, res, (error) => {
+        if (error) return next(error);
+        
+        console.log("User verification:", req.user);
+        
+        if (req.user?.id === req.params.id || req.user?.isAdmin) {
+            next();
+        } else {
+            return next(createError(403, "You are not authorized!"));
+        }
+    });
+};
+
+// Middleware to verify admin privileges
+export const verifyAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        console.log("Admin verification:", req.user);
+        
+        if (req.user?.isAdmin) {
+            next();
+        } else {
+            return next(createError(403, "You are not authorized!"));
+        }
     });
 };
