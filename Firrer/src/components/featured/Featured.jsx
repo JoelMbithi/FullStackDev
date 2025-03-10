09433de3d49fa9f9f338a9e716f960834ad1e623
Joel Mbithi <joellembithi@gmail.com>
2025-03-11 02:52:43 +0300

    Doinf Featured

---
+++ Firrer/src/components/featured/Featured.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./Featured.scss"
+import profileImage from "../../assets/back.jpeg"
+
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+      <div className="container">
+        <div className="left">
+            <h1>Find The perfect Freelance service for your business</h1>
+            <div className="search">
+                <div className="searchInput">
+                    <img src="" alt="" />
+                    <input type="text" placeholder='Try "building mobile app"' />
+                </div>
+                <button>search</button>
+            </div>
+            <div className="popular">
+                <span>Popular:</span>
+                <button>Web Design</button>
+                <button>WorkPress</button>
+                <button>Logo Design</button>
+                <button>AI service</button>
+            </div>
+        </div>
+        <div className="right">
+            <img src={profileImage} alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
