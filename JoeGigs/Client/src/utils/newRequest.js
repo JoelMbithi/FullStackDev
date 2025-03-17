a458b68ce84e00a8ac20b863ac8b5d31b850a053
Joel Mbithi <joellembithi@gmail.com>
2025-03-17 23:30:50 +0300

    doing the gig part where im dealing with the api to update the price search and sort

--- JoeGigs/Client/src/utils/newRequest.js
+++ JoeGigs/Client/src/utils/newRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const newRequest = axios.create({
-    baseURL: "http://localhost:8000/api/"
+    baseURL: "http://localhost:8000/api",
+    withCredentials: true 
 })
 
 export default newRequest
