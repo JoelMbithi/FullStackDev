8ea4e683c30ef49e413d06e8b58274cdd5ae5e57
Joel Mbithi <joellembithi@gmail.com>
2025-03-18 14:07:26 +0300

    Doing  Single Gig page to fetch all data from DB

--- JoeGigs/Client/src/components/gigCard/GigCard.jsx
+++ JoeGigs/Client/src/components/gigCard/GigCard.jsx
@@ -1,49 +1,77 @@
 import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
-import star from "../../assets/star.png";
-import heart from "../../assets/heart.png";
-import "./GigCard.scss";
-import { useQuery } from '@tanstack/react-query';
-import newRequest from '../../utils/newRequest';
+import star from "../../assets/star.png"; 
+import heart from "../../assets/heart.png"; 
+import "./GigCard.scss"; 
+import { useQuery } from '@tanstack/react-query'; 
+import noavatar from "../../assets/noavatar.jpg"
+import newRequest from '../../utils/newRequest'; 
 
+//display gig item from the gigs
 const GigCard = ({ item }) => {
+  
+  // Fetch user details based on the userId of the gig owner
+   // Unique query key to cache user data
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["gigsUser", item.userId],
+    // API call to get user details
+    queryFn: () => newRequest.get(`/user/getUser/${item.userId}`).then(res => res.data), 
+  });
 
-  const {isLoading, error, data} = useQuery({
-    queryKey:["gigsUser",],
-    queryFn: ()=> 
-      newRequest.get(`/user/getUser/${item.userId}`)
-  })
+  // Log user data whenever it updates (useful for debugging)
   useEffect(() => {
     console.log("User data:", data);
   }, [data]);
+
   return (
-    <Link to="/Gig/123" className="link">
-    <div className='gigCard'>
-     
+    <Link to={`/gig/${item._id}`} className="link"> 
+      {/* Clicking the card navigates to the gig details page */}
+      <div className='gigCard'>
+
+        {/* Display the gig's cover image */}
         <img src={item.cover} alt={item.title} />
+
         <div className="info">
-         { isLoading ? "loading" 
-         : error ? "Something went wrong!"
-         :  <div className="user">
-            <img src={data.img} alt=""/>
-            <span>{data.username}</span>
-          </div>}
+          {/* Show loading message, error message, or user details */}
+          {isLoading ? "loading..." 
+            : error ? "Something went wrong!" 
+            : <div className="user">
+                {/* Display user profile image and username */}
+                <img src={data?.img || {noavatar}} alt="User Profile" />
+                <span>{data?.username}</span>
+              </div>
+          }
+
+          {/* Display the gig description */}
           <p>{item.desc}</p>
+
+          {/* Display star rating with an icon */}
           <div className="star">
-            <img src={star} alt="Star Rating" />
-            <span>{item.rating}</span>
-          </div>
+  <img src={star} alt="Star Rating" />
+  <span>
+    {item.starNumber > 0 
+      ? Math.min(5, Math.round((item.totalStars / item.starNumber) * 5)) 
+      : "No Ratings Yet"}
+  </span>
+</div>
+
+
         </div>
-        <hr />
+
+        <hr /> 
+
+        {/* Display gig pricing and favorite (wishlist) option */}
         <div className="details">
-          <img src={heart} alt="Favorite" />
+          {/* Heart icon for saving gig */}
+          <img src={heart} alt="Favorite" /> 
           <div className="price">
             <span>STARTING AT</span>
-            <h2>${item.price}</h2>
+            {/* Displaying the gig price */}
+            <h2>${item.price}</h2> 
           </div>
         </div>
-     
-    </div>
+
+      </div>
     </Link>
   );
 };
